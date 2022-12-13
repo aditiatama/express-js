32fbf1dbad7de95c489c972953278d2eb8a4f7f9
arif.ramadhani <ariframadhani@uac.edu.au>
2022-12-13 13:56:49 +0700

    Fix endpoint method and response status

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -2,12 +2,12 @@ import express from "express";
 import UsersController from "../controllers/UsersController";
 const router = express.Router();
 
-router.route("/").post(UsersController.create).put(UsersController.getAll);
+router.route("/").post(UsersController.create).get(UsersController.getAll);
 
 router
   .route("/:userName")
   .get(UsersController.getByName)
-  .delete(UsersController.update)
-  .get(UsersController.delete);
+  .put(UsersController.update)
+  .delete(UsersController.delete);
 
 export default router;
